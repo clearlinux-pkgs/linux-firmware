fc737cdad096a0057e59f7ead9647d120a06defd
Arjan van de Ven <arjan@linux.intel.com>
2017-01-12 15:51:31 +0000

    Add update.sh for automatic following of upstream

---
+++ update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+VERSION=`curl -s https://git.kernel.org/cgit/linux/kernel/git/firmware/linux-firmware.git/commit/ | grep parent | cut -f6 -d">" | cut -f1 -d"<"`
+OLDVERSION=`cat linux-firmware.spec | head -1 | cut -f3 -d" "`
+
+if [ "$VERSION" == "$OLDVERSION" ] ; then
+	echo "Nothing changed $OLDVERSION == $VERSION"
+	exit 0
+fi
+	
+
+echo "Updating from $OLDVERSION to $VERSION"
+
+sed -i "s/$OLDVERSION/$VERSION/g" linux-firmware.spec
+make generateupstream
+git commit -a -m "Update to upstream commit $VERSION"
